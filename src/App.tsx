a78a25d7c821311377666e191c31e7b3f24dc139
Malik Bagwala <m.bagwala@gmail.com>
2025-01-09 13:44:49 +0530

    chore: finish infinite scrolling feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 type ItemType = {
   id: number;
   title: string;
 };
 
-function generateItems(prevId: number = 0, size: number = 13) {
+function getSentinal() {
+  return document.getElementById("card-sentinal");
+}
+function generateItems(page: number = 1, size: number = 13) {
   return new Promise<ItemType[]>((resolve) => {
     setTimeout(() => {
       return resolve(
         Array.from({ length: size }).map((_, index) => {
-          const newId = prevId + index + 1;
+          const newId = (page - 1) * size + (index + 1);
           return {
             id: newId,
             title: `Item ${newId}`,
@@ -23,6 +26,24 @@ function generateItems(prevId: number = 0, size: number = 13) {
 function App() {
   const [isLoading, setLoading] = useState(false);
   const [items, setItems] = useState<ItemType[]>([]);
+  const [page, setPage] = useState(1);
+
+  const observerCallback = useCallback<IntersectionObserverCallback>(
+    async (entries) => {
+      const sentinal = entries[0];
+      if (sentinal.isIntersecting) {
+        const newPage = page + 1;
+        setPage(newPage);
+        setLoading(true);
+        const response = await generateItems(newPage);
+        console.log("INTERSECTING", newPage, response);
+        setLoading(false);
+        setItems((prev) => prev.concat(response));
+      }
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [page, items.length]
+  );
   useEffect(() => {
     async function fetchInitialData() {
       setLoading(true);
@@ -30,26 +51,20 @@ function App() {
       setLoading(false);
       setItems(response);
     }
-    const observer = new IntersectionObserver(
-      async (entries) => {
-        const sentinal = entries[0];
-        if (sentinal.isIntersecting) {
-          setLoading(true);
-          const response = await generateItems(items.at(-1)?.id);
-          setLoading(false);
-          setItems((prev) => prev.concat(response));
-          // setItems((prev) => {
-          //   const lastItem = prev.at(-1);
-          //   return prev.concat(generateItems(lastItem?.id));
-          // });
-        }
-      },
-      { threshold: 1 }
-    );
-    const sentinalElement = document.getElementById("card-sentinal");
-    if (sentinalElement) observer.observe(sentinalElement);
     fetchInitialData();
   }, []);
+
+  useEffect(() => {
+    const sentinalElement = getSentinal();
+    const observer = new IntersectionObserver(observerCallback, {
+      threshold: 1,
+    });
+    if (sentinalElement) observer.observe(sentinalElement);
+
+    return function cleanup() {
+      observer.disconnect();
+    };
+  }, [observerCallback]);
   return (
     <div className="app">
       <div className="sidebar">
@@ -67,9 +82,9 @@ function App() {
         ))}
         {isLoading ? (
           <div>Loading...</div>
-        ) : items.length ? (
-          <div id="card-sentinal"></div>
-        ) : null}
+        ) : (
+          items.length && <div id="card-sentinal"></div>
+        )}
       </div>
       <div className="notes">
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima
